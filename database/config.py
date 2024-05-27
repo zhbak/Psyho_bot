f0475328b2c0d240f043c457842713fa2cd22bb0
Kirill Zhbakov <zhbakov@live.com>
2024-05-27 13:09:44 +0300

    database config

--- database/config.py
+++ database/config.py
@@ -7,7 +7,11 @@ load_dotenv()
 redis_host = os.environ.get("REDIS_HOST")
 redis_port = os.environ.get("REDIS_PORT")
 
-# postgres Settings
+
+# Load environment variables from "stack.env"
+load_dotenv("stack.env")
+
+# Define settings using Pydantic's BaseSettings
 class Settings(BaseSettings):
     DB_HOST_SQL: str
     DB_PORT_SQL: int
@@ -18,7 +22,25 @@ class Settings(BaseSettings):
     @property
     def DATABASE_URL_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USER_SQL}:{self.DB_PASS_SQL}@{self.DB_HOST_SQL}:{self.DB_PORT_SQL}/{self.DB_NAME}"
-    
+
+"""
+# Instantiate the settings
+settings = Settings()
+
+# postgres Settings
+class Settings(BaseSettings):
+    DB_HOST_SQL: str
+    DB_PORT_SQL: int
+    DB_USER_SQL: str
+    DB_PASS_SQL: str
+    DB_NAME: str
     model_config = SettingsConfigDict(env_file="stack.env", extra="ignore")
 
+    @property
+    def DATABASE_URL_asyncpg(self):
+        return f"postgresql+asyncpg://{self.DB_USER_SQL}:{self.DB_PASS_SQL}@{self.DB_HOST_SQL}:{self.DB_PORT_SQL}/{self.DB_NAME}"
+    
+    
+
 settings = Settings()
+"""
