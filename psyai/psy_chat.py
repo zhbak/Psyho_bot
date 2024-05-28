ffe05a14d5933246a74cc63cf5ec449745f4d095
Kirill Zhbakov <zhbakov@live.com>
2024-05-28 11:41:32 +0300

    update

--- psyai/psy_chat.py
+++ psyai/psy_chat.py
@@ -3,7 +3,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.memory import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory, RunnablePassthrough
 #from langchain_community.chat_message_histories import RedisChatMessageHistory
-from psyai.psy_chat import RedisChatMessageHistory
+from psyai.redis_chat import RedisChatMessageHistory
 from dotenv import load_dotenv
 from database.database import redis_url
 from database.orm import execute_redis_command
