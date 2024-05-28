110242277cf28a278b96741f5a0aa91b15b1af62
Kirill Zhbakov <zhbakov@live.com>
2024-05-28 12:03:52 +0300

    update

--- psyai/psy_chat.py
+++ psyai/psy_chat.py
@@ -83,7 +83,7 @@ async def psyho_chat(system_prompt, user_input, pool, chat_id, chat):
 
     chain_with_message_history = RunnableWithMessageHistory(
             chain,
-            get_message_history,
+            get_message_history(pool),
             input_messages_key="input",
             history_messages_key="chat_history"
         )
