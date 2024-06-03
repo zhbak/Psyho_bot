cb6b784d6b70c603ebc99dd5103ab5a49fc746de
Kirill Zhbakov <zhbakov@live.com>
2024-06-03 15:05:56 +0300

    updt

--- bot.py
+++ bot.py
@@ -19,10 +19,9 @@ async def start_bot():
     while True:
         try:
             await config.bot.polling(non_stop=True)
-            config.bot.polling()
         except Exception as e:
             logger.exception("Exception occurred: %s", e)
-            await asyncio.sleep(5)  # Пауза перед следующей попыткой
+            await asyncio.sleep(1)  # Пауза перед следующей попыткой
 
 async def main():
     await orm.create_tables()
@@ -32,7 +31,7 @@ async def main():
             await start_bot()
         except Exception as e:
             logger.exception("Exception occurred: %s", e)
-            await asyncio.sleep(5)  # Пауза перед следующей попыткой
+            await asyncio.sleep(1)  # Пауза перед следующей попыткой
 
 # Запуск бота
 if __name__ == "__main__":
